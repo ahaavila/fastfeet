30aa04e9d285b78c21fdb8c54b8eb8799638b4ce
Augusto <aavila@verointernet.com.br>
2020-02-06 21:37:13 -0300

    Adicionado eslint, prettier e editorConfig

--- src/routes.js
+++ src/routes.js
@@ -3,7 +3,7 @@ import { Router } from 'express';
 const routes = new Router();
 
 routes.get('/', (req, res) => {
-  res.json({ message: "ok" });
+  res.json({ message: 'ok' });
 });
 
 export default routes;
